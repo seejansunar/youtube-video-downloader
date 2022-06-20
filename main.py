f83270c3c91585ba822818f93c69b64cec661f94
seejansunar <czansunar0098@gmail.com>
2022-06-20 22:02:54 +0545

    resolved #3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+from pytube import Channel
 
 # Create youtube object called yt
 yt = YouTube("https://www.youtube.com/watch?v=PKtnafFtfEo")
@@ -6,3 +7,7 @@ yt = YouTube("https://www.youtube.com/watch?v=PKtnafFtfEo")
 yt.streams.first().download()
 print(yt.captions)
 print(yt.thumbnail_url)
+
+c = Channel('https://www.youtube.com/c/ProgrammingKnowledge')
+for url in c.video_urls[:5]:
+    print(url)
