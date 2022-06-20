f54aa5d219c4b5ba0161176d092d84775563f4f8
seejansunar <czansunar0098@gmail.com>
2022-06-20 22:04:26 +0545

    resolved #4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 from pytube import Channel
+from pytube import Playlist
 
 # Create youtube object called yt
 yt = YouTube("https://www.youtube.com/watch?v=PKtnafFtfEo")
@@ -10,4 +11,8 @@ print(yt.thumbnail_url)
 
 c = Channel('https://www.youtube.com/c/ProgrammingKnowledge')
 for url in c.video_urls[:5]:
+    print(url)
+
+p = Playlist('https://www.youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n')
+for url in p.video_urls[:5]:
     print(url)
